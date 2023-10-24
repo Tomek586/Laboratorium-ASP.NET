f23c01e86f397663181aefc7c73095b122b70c1a
Tomek586 <tomasz.dziedzic@microsoft.wsei.edu.pl>
2023-10-24 14:46:23 +0200

    Add project files.

---
+++ Laboratorium 3 - App/Controllers/ContactController.cs
@@ -0,0 +1,72 @@
+﻿using Laboratorium_3___App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3___App.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_contacts.FindAll());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactService.Add(model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_contacts[id]);
+
+        }
+        [HttpGet]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contacts[model.Id] = model;
+            }
+            return View();
+
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_contacts[id]);
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_contacts[id]);
+
+        }
+
+
+
+    }
+}
---
+++ Laboratorium 3 - App/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3___App.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Musisz wpisać imię!")]
+        [StringLength(maximumLength:100, ErrorMessage ="Zbyt długie imię, maksymalnie 100 znaków!")]
+        [Display (Name ="Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Musisz podać e-mail!")]
+        [EmailAddress(ErrorMessage ="Niepoprawny adres e-mail, brak znaku @")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Phone(ErrorMessage ="Niepoprawny numer telefonu!")]
+        public string Phone { get; set; }
+        public DateTime Birth {  get; set; }
+
+        [Display(Name = "Priorytet")]
+        public Priority Priority { get; set; }
+
+    }
+}
